cf4dddfe936bd84effb63809f633b86cc6db914f
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-10-22 23:38:33 +0100

    fix(frontend): avoid linter warning

--- zuko-frontend/src/main.tsx
+++ zuko-frontend/src/main.tsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import "./css/index.css";
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('No root element found')
+}
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
